bac7402e7cefa86dfcdc44fcb132dcb640673a0f
seymenklc <seymennklc@gmail.com>
2024-03-17 01:56:51 +0300

    fix: mobile session card

--- src/app/_components/sessions.tsx
+++ src/app/_components/sessions.tsx
@@ -105,7 +105,7 @@ export default function Sessions() {
                                  {title}
                               </p>
                               <p className="text-sm text-gray-500 flex items-center gap-1">
-                                 {moment(formatFirebaseTimestamp(createdAt)).format('MMM DD, YYYY - hh:mm A')}
+                                 {moment(formatFirebaseTimestamp(createdAt)).format('MMM DD, YYYY')}
                               </p>
                            </div>
                            <div className="text-lg font-semibold">
