3ec2b8df89483cef22b2e159c6b27b26893271fa
seymenklc <seymennklc@gmail.com>
2024-03-17 01:44:19 +0300

    fix: try force mantine theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-"use client"
-import "./globals.css";
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
+import "./globals.css";
 import { Roboto } from "next/font/google";
 import React from 'react'
 import BaseProvider from "./_components/providers/base-provider";
 import BaseLayout from "./_components/layouts/base-layout";
 import { ColorSchemeScript } from "@mantine/core";
-import { unstable_noStore } from "next/cache";
 
 const roboto = Roboto({
   weight: "400",
@@ -16,20 +14,17 @@ const roboto = Roboto({
 });
 
 export default function RootLayout(props: Readonly<React.PropsWithChildren>) {
-  unstable_noStore()
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme="light" />
       </head>
       <body className={`h-screen font-sans ${roboto.variable}`}>
-        <div vaul-drawer-wrapper="">
-          <BaseProvider>
-            <BaseLayout>
-              {props.children}
-            </BaseLayout>
-          </BaseProvider>
-        </div>
+        <BaseProvider>
+          <BaseLayout>
+            {props.children}
+          </BaseLayout>
+        </BaseProvider>
       </body>
     </html>
   );
